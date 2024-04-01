ca2dc2c810a7fd5f5387519d0c7ff29490497819
shane <shanebrouwer@gmail.com>
2024-04-01 18:24:22 +1100

    z

--- nodes/Liselot (2).gd
+++ nodes/Liselot (2).gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
+
 @onready var axis = Vector2.ZERO
 
-var SPEED = 50.0
-var JUMP_VELOCITY = -400.0
+const SPEED = 100.0
+const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var direction = -1
+
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -16,40 +17,19 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
-
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction = Input.get_axis("move_left", "move_right")
-
-	var sizeX = get_viewport().size.x / 2
-	#print (get_viewport().size, get_viewport().sdf_oversize)
-
-	if (position.x < 0):
-		direction *= -1
-		position.x = 1
-		#$SndTalk.play()
-		
-	if (position.x > sizeX):
-		direction *= -1
-		position.x = sizeX - 5
-		#$SndTalk.play()
-		
-		
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
+		#acceleration.x = 30
 		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-	
-	var push_force = 5
-	for i in get_slide_collision_count():
-		var c = get_slide_collision(i)
-		if c.get_collider() is RigidBody2D:
-			c.get_collider().apply_central_impulse(-c.get_normal() * push_force)
+
 
 
 
@@ -62,19 +42,16 @@ func _ready():
  #Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if (velocity.y < 0):
-		$CharacterAnimations.play("jump")
+		$LiselotAnimations.play("jump")
 	elif (abs(velocity.x) > 0):
-		$CharacterAnimations.play("run")
+		$LiselotAnimations.play("run")
 	else:
-		$CharacterAnimations.play("idle")
+		$LiselotAnimations.play("idle")
 		
 	if (velocity.x>0):
-		$CharacterAnimations.flip_h = 0
+		$LiselotAnimations.flip_h = 0
 	if (velocity.x<0):
-		$CharacterAnimations.flip_h = 1
-		
-	if (position.y > 1080):
-		position.y=0
+		$LiselotAnimations.flip_h = 1
 	#axis.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	#move_and_slide()
 	
--- nodes/Liselot.gd
+++ nodes/Liselot.gd
@@ -1,59 +1,10 @@
-extends CharacterBody2D
-@onready var axis = Vector2.ZERO
+extends "res://nodes/ComputerControlledPlayer.gd"
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -300.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
-		#acceleration.x = 30
-		
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
-
-
-
-
-
-# Called when the node enters the scene tree for the first time.
+func _init():
+	SPEED=125
+	
 func _ready():
 	pass # Replace with function body.
-
-
- #Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if (velocity.y < 0):
-		$LiselotAnimations.play("jump")
-	elif (abs(velocity.x) > 0):
-		$LiselotAnimations.play("run")
-	else:
-		$LiselotAnimations.play("idle")
-		
-	if (velocity.x>0):
-		$LiselotAnimations.flip_h = 0
-	if (velocity.x<0):
-		$LiselotAnimations.flip_h = 1
-	#axis.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
-	#move_and_slide()
-	
 
 
 
