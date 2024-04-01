203e3e9508ca1c1b18f448f14d44f9d2c880a684
shane <shanebrouwer@gmail.com>
2024-04-01 23:04:27 +1100

    x

--- nodes/Door.gd
+++ nodes/Door.gd
@@ -4,7 +4,12 @@ func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		if get_rect().has_point(to_local(event.position)):
 			print("A click!")
-			
+			if (frame==0):
+				frame = 2
+				$CloseDoor.play()
+			elif (frame==2):
+				frame=0
+				$OpenDoor.play()
 
 #func _input(event):
 	#if event is InputEventMouseButton and event.pressed:
