5de2b83efd2a3b21e3a0d434d524602a3e1a4d64
shane <shanebrouwer@gmail.com>
2024-05-07 17:04:48 +1000

    x

--- nodes/PointBurst.gd
+++ nodes/PointBurst.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
+@onready var velocity = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	velocity = 1.0 + (randf()) 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.y += 1;
+	position.y -= velocity;
